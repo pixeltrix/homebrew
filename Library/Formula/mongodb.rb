41d8e5268807bdc2695f7044d97397cf6b285378
Andrew White <andyw@pixeltrix.co.uk>
2009-12-31 07:37:49 +0000

    Update MongoDB formula to 1.2.1

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -7,13 +7,13 @@ class Mongodb <Formula
   aka :mongo
 
   if Hardware.is_64_bit? and not ARGV.include? '--32bit'
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.2.0.tgz'
-    md5 'e2d6f7b7f1c0ab71fd629c015ced033c'
-    version '1.2.0-x86_64'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.2.1.tgz'
+    md5 'b5ac0642a6e370b0c4bd24157445c96e'
+    version '1.2.1-x86_64'
   else
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.2.0.tgz'
-    md5 'e85c3e4bdf910ece0e47f85885b78219'
-    version '1.2.0-i386'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.2.1.tgz'
+    md5 'f7707bb5756380a580c08fc11f0a351a'
+    version '1.2.1-i386'
   end
 
   def skip_clean? path
